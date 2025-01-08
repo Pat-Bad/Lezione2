b77a319adf7836a77d64e06368795a5185b6f431
Patricia <patricia.badji89@gmail.com>
2025-01-08 15:56:55 +0100

    Update whileEx.java

--- src/Esercizio3/whileEx.java
+++ src/Esercizio3/whileEx.java
@@ -2,14 +2,23 @@ package Esercizio3;
 //chiede una stringa e la ritorna in caratteri separati da una virgola//
 
 public class whileEx {
-    public static String separatedByCommas (String word) {
+    public static void main(String[] args)
+    {
 
-        char[] characters = word.toCharArray();
-       return characters.toString();
+        String string = "Pasquale";
+        int i=0;
+
+
+        System.out.println(
+                "Individual characters from given string: ");
+
+        // Iterate through the given string to
+        // display the individual characters
+
+            while (i<string.length()){System.out.println((string.charAt(i) + ", "));
+                    if (string.charAt(i) == 'q') {break;}
+                    i++;
+        }
+
     }
-
-public static void main(String[] args) {
-        System.out.println(separatedByCommas("ciao"));
-
-}
 }
