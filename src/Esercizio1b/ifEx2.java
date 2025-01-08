c520ff9c71c12065658bdb43749e880b548fd4ff
Patricia <patricia.badji89@gmail.com>
2025-01-08 17:37:07 +0100

    Update ifEx2.java

--- src/Esercizio1b/ifEx2.java
+++ src/Esercizio1b/ifEx2.java
@@ -2,12 +2,14 @@ package Esercizio1b;
 
 public class ifEx2 {
     public static boolean annoBisestile (int year) {
-       if (year % 4 == 0 ) {
-           if (year % 100 == 0) {
-               return year % 400 == 0;
-           }
-       else {return true;}} else {return false;}}
-
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     public static void main(String[] args) {
     System.out.println(annoBisestile(2024));
     System.out.println(annoBisestile(1989));}
